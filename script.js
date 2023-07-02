b7377ebbc827156084796acf7b85ce10df18b85d
Baljeet Singh <138297578+ballumbbs@users.noreply.github.com>
2023-07-02 16:36:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// JavaScript code
+function goToPage() {
+    // Redirect to the specified HTML file
+    window.location.href = "abc.html";
+  }
+
+  function newpage() {
+    // Redirect to the specified HTML file
+    window.location.href = "def.html";
+  }
+
+  function website() {
+    // Redirect to the specified HTML file
+    window.location.href = "https://wandererguy.in";
+  }
+
+  
